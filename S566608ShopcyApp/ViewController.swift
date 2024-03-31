8d30965f7b9dd13376f44472a0ad980972d02f8b
Bhargavi Tatineni <s566608@nwmissouri.edu>
2024-03-31 03:21:21 -0700

    Initial Commit

---
+++ S566608ShopcyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  S566608ShopcyApp
+//
+//  Created by Bhargavi Tatineni on 3/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
