8f75939115cd0ef7cfedb8b4dd4ca449fded8029
Bhargavi Tatineni <s566608@nwmissouri.edu>
2024-03-31 13:18:12 -0700

    UI

---
+++ S566608ShopcyApp/ProductView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductView.swift
+//  S566608ShopcyApp
+//
+//  Created by Bhargavi Tatineni on 3/31/24.
+//
+
+import UIKit
+
+class ProductView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
